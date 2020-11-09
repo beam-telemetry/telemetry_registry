94117d31d657dc72f426a27a5acf9aee0888577d
Dave Lucia <davelucianyc@gmail.com>
2020-11-08 22:26:47 -0500

    Fix ordering of telemetry events at compile-time (#9)
    Fixes the ordering when using the `telemetry_docs` macro, since module
    attributes are collected and stored in reverse order.

--- test/support/test_elixir_app.ex
+++ test/support/test_elixir_app.ex
@@ -2,7 +2,14 @@ defmodule TestElixirApp do
   use TelemetryRegistry
 
   telemetry_event %{
-    event: [:test_elixir_app, :single, :event],
+    event: [:test_elixir_app, :event, :start],
+    description: "emitted when this event happens",
+    measurements: "%{duration: non_neg_integer()}",
+    metadata: "%{status: status(), name: String.t()}"
+  }
+
+  telemetry_event %{
+    event: [:test_elixir_app, :event, :stop],
     description: "emitted when this event happens",
     measurements: "%{duration: non_neg_integer()}",
     metadata: "%{status: status(), name: String.t()}"
